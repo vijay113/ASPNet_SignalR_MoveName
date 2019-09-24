107094719819398141d03e7399ab52652bd36dff
vijay113 <vijay113kumar@gmail.com>
2019-09-24 10:31:54 +0530

    Repo git Init

---
+++ ASPNet_MoveShapeDemo_SignalR/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(ASPNet_MoveShapeDemo_SignalR.Startup))]
+namespace ASPNet_MoveShapeDemo_SignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
